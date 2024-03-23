646c675fb27fad24a47e85b51a544dc3d2112a87
songusera <202300300005@mail.sdu.edu.cn>
2024-03-23 16:01:52 +0800

    course

--- src/main/java/org/example/server/mapper/CourseMapper.java
+++ src/main/java/org/example/server/mapper/CourseMapper.java
@@ -1,11 +1,13 @@
 package org.example.server.mapper;
 
 import org.apache.ibatis.annotations.Mapper;
+import org.example.server.payload.request.DataRequest;
 import org.example.server.pojo.Course;
 
 import java.util.List;
 
 @Mapper
 public interface CourseMapper {
-    List<Course> updateInfo();
+    void updateInfo(Integer id, String course_name,Double credit,Integer num,Integer course_type_id,Integer pre_course_id,String book,String extracurricular);
+    List<Course> selectAll();
 }
