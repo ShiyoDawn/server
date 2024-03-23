52c03c8879939d26e252d0bfe7cb63dfa8331c17
SuperHZX666 <202300300017@mail.sdu.edu.cn>
2024-03-23 15:36:59 +0800

    增加了person的Mapper，Service

---
+++ src/main/java/org/example/server/mapper/PersonMapper.java
@@ -0,0 +1,13 @@
+package org.example.server.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.example.server.pojo.Person;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface PersonMapper extends BaseMapper<Person> {
+    List<Person> findByPersonName(String person_name);
+    Optional<Person> findById(Integer id);
+    Optional<Person> findByPersonNum(String person_num);
+}
