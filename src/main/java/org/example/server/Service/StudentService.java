f3ce978b615c8691d9e0881b93b7ce5434f11a85
SuperHZX666 <202300300017@mail.sdu.edu.cn>
2024-03-26 11:58:18 +0800

    编写了通过学生姓名或通过部门查找信息的功能
    对Student添加了一些内容，编写了通过学生姓名或通过部门查找信息的功能，并将其转成Map-List，方便controller中调用并传输

--- src/main/java/org/example/server/Service/StudentService.java
+++ src/main/java/org/example/server/Service/StudentService.java
@@ -1,15 +1,71 @@
 package org.example.server.Service;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import org.example.server.mapper.StudentMapper;
 import org.example.server.pojo.Person;
 import org.example.server.pojo.Student;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.validation.constraints.Max;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 @Service
 public class StudentService {
+    @Autowired
+    private StudentMapper studentMapper;
+    public List<Student> searchStudentsByName(String student_Name){
+//        QueryWrapper<Student> queryWrapper = new QueryWrapper<>();
+//        queryWrapper.like("student_name", student_Name);
+        //上面两行是mp提供的模糊搜索，试了一下但不太会用，感觉没必要，以后再说；
+        List<Student> students = studentMapper.findByStudentName(student_Name);
+        return students;
+    }
+    public List getStudentMapList(List<Student> students){
+        List list = new ArrayList();
+        if (students == null || students.size() == 0)
+            return list;
+        Map m;
+        for(Student s:students){
+            m = new HashMap();
+            m.put("id",s.getId());
+            m.put("person_id",s.getPerson_id());
+            m.put("student_name",s.getStudent_name());
+            m.put("department",s.getDepartment());
+            m.put("classes",s.getClasses());
+            m.put("grade",s.getGrade());
+            m.put("major",s.getMajor());
+            list.add(m);
+        }
+        return list;
+    }
+    public List<Student> searchStudentByDepartment(String department){
+        List<Student> departmentStudentMember = studentMapper.findByDepartment(department);
+        return departmentStudentMember;
+    }
+    public List getDepartmentMemberList(List<Student> departmentStudentMember){
+        List list = new ArrayList();
+        if (departmentStudentMember == null || departmentStudentMember.size() == 0)
+            return list;
+        Map m;
+        for(Student s:departmentStudentMember){
+            m = new HashMap();
+            m.put("id",s.getId());
+            m.put("person_id",s.getPerson_id());
+            m.put("student_name",s.getStudent_name());
+            m.put("department",s.getDepartment());
+            m.put("classes",s.getClasses());
+            m.put("grade",s.getGrade());
+            m.put("major",s.getMajor());
+            list.add(m);
+        }
+        return list;
+    }
+
+
     public Map getMapFromStudent(Student student){
         Map map = new HashMap();
         if(student == null) {
