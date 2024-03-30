4950642af4a0457d924b3cae68a54e942dabb4eb
destinyon <why2017223584@163.com>
2024-03-30 17:07:08 +0800

    宋俊儒写的，不关我事qwq

--- src/main/java/org/example/server/controller/CourseController.java
+++ src/main/java/org/example/server/controller/CourseController.java
@@ -1,8 +1,9 @@
-/*
+
 package org.example.server.controller;
 
 import org.example.server.Service.CourseService;
 import org.example.server.payload.response.DataResponse;
+import org.example.server.pojo.Course;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -14,16 +15,26 @@ public class CourseController {
     @Autowired
     CourseService courseService;
     @PostMapping("/updateInfo")
-    public String updateInfo(@RequestParam Integer id, @RequestParam String course_name, @RequestParam Double credit, @RequestParam Integer num, @RequestParam Integer course_type_id, @RequestParam Integer pre_course_id, @RequestParam String book, @RequestParam String extracurricular){
-        return courseService.updateInfo(id, course_name,credit,num,course_type_id,pre_course_id,book,extracurricular);
-
-    public DataResponse updateInfo(@RequestParam Integer id, @RequestParam String course_name, @RequestParam Double credit, @RequestParam Integer num, @RequestParam Integer course_type_id, @RequestParam Integer pre_course_id, @RequestParam String book, @RequestParam String extracurricular){
-        return courseService.updateInfo(id, course_name,credit,num,course_type_id,pre_course_id,book,extracurricular);
+    public DataResponse updateInfo(@RequestParam Integer id, @RequestParam String course_name, @RequestParam Double credit, @RequestParam Integer num, @RequestParam Integer course_type_id, @RequestParam Integer pre_course_id, @RequestParam String book, @RequestParam String extracurricular) {
+        return courseService.updateInfo(id, course_name, credit, num, course_type_id, pre_course_id, book, extracurricular);
     }
     @PostMapping("/selectInfo")
     public DataResponse selectInfo(@RequestParam Integer id){
         return courseService.selectInfo(id);
     }
+    @PostMapping("/addCourse")
+    public DataResponse addCourse(@RequestParam String course_name, @RequestParam Double credit, @RequestParam Integer num, @RequestParam Integer course_type_id, @RequestParam Integer pre_course_id, @RequestParam String book, @RequestParam String extracurricular){
+        return courseService.addCourse(course_name, credit, num, course_type_id, pre_course_id, book, extracurricular);
+    }
+    @PostMapping("/deleteCourseById")
+    public DataResponse deleteCourseByID(@RequestParam Integer id){
+        return courseService.deleteCourseById(id);
+    }
+    @PostMapping("/selectMixed")
+    public DataResponse selectMixed(@RequestParam Course course ,@RequestParam(defaultValue = "1") Integer pageNum){
+        return DataResponse.success(courseService.selectMixed(course,pageNum));
+    }
+
 }
 
-*/
+
--- src/test/java/org/example/server/ServerApplicationTests.java
+++ src/test/java/org/example/server/ServerApplicationTests.java
@@ -1,7 +1,10 @@
 package org.example.server;
 
+import org.example.server.pojo.Course;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
 
 @SpringBootTest
 class ServerApplicationTests {
@@ -12,6 +15,19 @@ class ServerApplicationTests {
 
     @Test
     void select() {
+    }
+    @Test
+    void selectMixed(){
+        Integer id = null;
+        String course_name = "j";
+        Double credit = null;
+        Integer num = null;
+        Integer course_type_id = 2;
+        Integer pre_course_id = null;
+        String book = null;
+        String extracurricular = null;
+        Course course = new Course(id,course_name,credit, num, course_type_id, pre_course_id, book, extracurricular);
+//        selectMixed(course,1);
     }
 
 }
