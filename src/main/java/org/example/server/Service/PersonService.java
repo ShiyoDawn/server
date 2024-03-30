038b37564642d82b2cd21d74382c43d66d238db3
destinyon <why2017223584@163.com>
2024-03-30 17:54:02 +0800

    增加了person的相关方法功能

--- src/main/java/org/example/server/Service/PersonService.java
+++ src/main/java/org/example/server/Service/PersonService.java
@@ -1,6 +1,10 @@
 package org.example.server.Service;
 
 import org.example.server.mapper.PersonMapper;
+import org.example.server.mapper.StudentMapper;
+import org.example.server.mapper.UserMapper;
+import org.example.server.payload.response.DataResponse;
+import org.example.server.pojo.Person;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -8,5 +12,28 @@ import org.springframework.stereotype.Service;
 public class PersonService {
     @Autowired
     PersonMapper personMapper;//注入人员数据；
+    @Autowired
+    StudentMapper studentMapper;
+    @Autowired
+    UserMapper userMapper;
 
+    public DataResponse getPersonList() {
+        return DataResponse.success(personMapper.selectAll());
+    }
+
+    public DataResponse selectById(Integer id) {
+        Person person = personMapper.selectById(id);
+        if (person == null)
+            return DataResponse.error(404, "查询失败，此人不存在");
+        else
+            return DataResponse.success(person, "查询成功！");
+    }
+
+    public DataResponse selectByPersonNum(String person_num) {
+        Person person = personMapper.selectByPersonNum(person_num);
+        if (person == null)
+            return DataResponse.error(404, "查询失败，此人不存在");
+        else
+            return DataResponse.success(person, "查询成功！");
+    }
 }
