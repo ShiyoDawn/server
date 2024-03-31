8d80f82318f4a148701886b4464b10ca41b51f98
songusera <202300300005@mail.sdu.edu.cn>
2024-03-31 19:41:21 +0800

    lesson的添加更改作业，lesson表加入学期与状态，score表加入teacher_id

---
+++ src/main/java/org/example/server/mapper/LessonMapper.java
@@ -0,0 +1,14 @@
+package org.example.server.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
+import org.example.server.pojo.Lesson;
+
+import java.util.List;
+
+@Mapper
+public interface LessonMapper extends BaseMapper<Lesson> {
+    void updateHomework(String homework,Integer id);
+    List<Lesson> selectLesson();
+
+}
