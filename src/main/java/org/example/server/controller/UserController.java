c979fb016dc1075fec204cce2f3d4d2f02cdf78b
destinyon <why2017223584@163.com>
2024-05-09 15:47:04 +0800

    加了个文件专门存上传的照片，详细看user里的方法

--- src/main/java/org/example/server/controller/UserController.java
+++ src/main/java/org/example/server/controller/UserController.java
@@ -1,13 +1,14 @@
 package org.example.server.controller;
 
 import org.example.server.Service.UserService;
+import org.example.server.payload.Result;
 import org.example.server.payload.request.DataRequest;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.io.*;
+import java.util.Base64;
 
 @RestController
 @RequestMapping("/user")
@@ -20,5 +21,44 @@ public class UserController {
         String person_num = dataRequest.getString("person_num");
         String password = dataRequest.getString("password");
         userService.updatePassword(person_num, password);
+    }
+
+    @PostMapping(path = "/uploadPhoto")
+    public Result uploadPhoto(@RequestBody byte[] barr,
+                              @RequestParam(name = "remoteFile") String remoteFile,
+                              @RequestParam(name = "fileName") String fileName) {
+        try {
+            File file=new File("src/main/" + remoteFile+"/"+fileName);
+            System.out.println(file.getPath());
+            OutputStream os = new FileOutputStream(new File("src/main/" + remoteFile+"/"+fileName));
+            os.write(barr);
+            os.close();
+            return Result.ok();
+        } catch (Exception e) {
+            return Result.ok();
+        }
+
+    }
+
+    @PostMapping("/getPhoto")
+    public Result getPhoto(@RequestBody DataRequest dataRequest) {
+        String person_num = dataRequest.getString("person_num");
+        File file=new File("src/main/photo/"+person_num+".jpg");
+        if(!file.exists()) {
+            return Result.error(-1,"文件不存在");
+        }
+        int len = (int) file.length();
+        byte data[] = new byte[len];
+        FileInputStream in = null;
+        String imgstr=null;
+        try {
+            in = new FileInputStream(file);
+            in.read(data);
+            in.close();
+            imgstr=new String(Base64.getEncoder().encode(data));
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        return Result.success(imgstr);
     }
 }
